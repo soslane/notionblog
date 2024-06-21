62342bed67e6e91e39914cef65ef39a54e9f97ab
soslane <149466045+soslane@users.noreply.github.com>
2024-06-21 20:15:33 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -7,7 +7,7 @@ const CONFIG = {
     bio: "Fake it till you make it",
     email: "soslane@proton.me",
     linkedin: "",
-    github: "soslane",
+    github: "",
     instagram: "",
   },
   projects: [
@@ -24,8 +24,8 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://cloud.myla.eu.org",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
