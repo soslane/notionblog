1a1ba970af672a51d7f8ac3024a21b109c911858
soslane <149466045+soslane@users.noreply.github.com>
2024-06-21 20:19:25 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     name: "Gweek",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Cloud Computering Team",
-    bio: "Fake it till you make it",
+    bio: "",
     email: "soslane@proton.me",
     linkedin: "",
     github: "",
