8b696847a31310df8d4c434d4f81091f5d3ed997
soslane <149466045+soslane@users.noreply.github.com>
2024-06-29 22:23:07 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,13 +18,13 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "Steven",
+    title: "Steven Blog",
     description: "welcome to my blog !",
     scheme: "light", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://cloud.myla.eu.org",
+  link: "https://zyc.myla.eu.org",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
